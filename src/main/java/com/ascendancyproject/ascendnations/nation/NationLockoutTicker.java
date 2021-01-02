f44e7d321f63c8666e3654321877bd24e0cd0c7c
Harry Billings <harry@froogo.co.uk>
2021-01-02 10:56:39 +0000

    Added automatic chunk reclaiming
    Added automatic reclaiming of random chunks after lockout expires. I still need to add outposts.
    
    Related to issue #20.

--- src/main/java/com/ascendancyproject/ascendnations/nation/NationLockoutTicker.java
+++ src/main/java/com/ascendancyproject/ascendnations/nation/NationLockoutTicker.java
@@ -3,6 +3,10 @@ package com.ascendancyproject.ascendnations.nation;
 import com.ascendancyproject.ascendnations.AscendNations;
 import com.ascendancyproject.ascendnations.PersistentData;
 import com.ascendancyproject.ascendnations.language.Language;
+import com.ascendancyproject.ascendnations.rift.RiftConfig;
+
+import java.util.HashSet;
+import java.util.Iterator;
 
 public class NationLockoutTicker {
     public NationLockoutTicker(AscendNations plugin) {
@@ -14,8 +18,30 @@ public class NationLockoutTicker {
             if (nation.getPower().getLockoutExpiry() != 0)
                 nation.broadcast(Language.format("nationLockoutReminder", new String[]{"nationName", nation.getName()}));
 
-            if (nation.getPower().getChunkLockoutExpiry() != 0)
-                nation.broadcast(Language.format("nationLockoutChunkReminder", new String[]{"nationName", nation.getName()}));
+            if (nation.getPower().getChunkLockoutExpiry() != 0) {
+                if (nation.getPower().getChunkLockoutExpiry() < System.currentTimeMillis())
+                    reclaimChunks(nation);
+                else
+                    nation.broadcast(Language.format("nationLockoutChunkReminder", new String[]{"nationName", nation.getName()}));
+            }
         }
+    }
+
+    private void reclaimChunks(Nation nation) {
+        int startingChunks = nation.getChunks().size();
+
+        while (nation.getPower().getChunkLockoutExpiry() != 0) {
+            Iterator<Long> it = nation.getChunks().iterator();
+            Long remove = it.next();
+            if (nation.isClaimChunk(remove) || RiftConfig.getRift(remove) != null)
+                remove = it.next();
+
+            HashSet<Long> touched = nation.getTouched(remove);
+            nation.recalculateChunks(touched);
+
+            nation.getPower().recalculate(nation);
+        }
+
+        nation.broadcast(Language.format("nationReclaimedChunks", new String[]{"chunkCount", Integer.toString(startingChunks - nation.getChunks().size())}));
     }
 }
