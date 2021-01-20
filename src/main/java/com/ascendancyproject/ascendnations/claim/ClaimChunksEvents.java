5fa8e94a9dd4f5caa7805483ea4bec8220171947
Harry Billings <harry@froogo.co.uk>
2021-01-20 02:24:17 +0000

    Fixed claim message teleportation bug
    Fixed a bug where claim (entry/exit) messages would not show up when the player teleported.
    
    Also added ignoreCancelled and high priority to these events, as they should execute later.

--- src/main/java/com/ascendancyproject/ascendnations/claim/ClaimChunksEvents.java
+++ src/main/java/com/ascendancyproject/ascendnations/claim/ClaimChunksEvents.java
@@ -4,9 +4,13 @@ import com.ascendancyproject.ascendnations.AscendNations;
 import com.ascendancyproject.ascendnations.PersistentData;
 import com.ascendancyproject.ascendnations.language.Language;
 import com.ascendancyproject.ascendnations.nation.Nation;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
 
 import java.util.UUID;
 
@@ -15,22 +19,37 @@ public class ClaimChunksEvents implements Listener {
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
 
-    @EventHandler
+    @EventHandler(
+            ignoreCancelled = true,
+            priority = EventPriority.HIGH
+    )
     public void onPlayerMove(PlayerMoveEvent event) {
-        if (event.getFrom().getChunk().equals(event.getTo().getChunk()))
+        move(event.getPlayer(), event.getFrom(), event.getTo());
+    }
+
+    @EventHandler(
+            ignoreCancelled = true,
+            priority = EventPriority.HIGH
+    )
+    public void onPlayerTeleport(PlayerTeleportEvent event) {
+        move(event.getPlayer(), event.getFrom(), event.getTo());
+    }
+
+    private void move(Player player, Location from, Location to) {
+        if (from.getChunk().equals(to.getChunk()))
             return;
 
-        UUID fromUUID = ClaimChunks.chunks.get(event.getFrom().getChunk().getChunkKey());
-        UUID toUUID = ClaimChunks.chunks.get(event.getTo().getChunk().getChunkKey());
+        UUID fromUUID = ClaimChunks.chunks.get(from.getChunk().getChunkKey());
+        UUID toUUID = ClaimChunks.chunks.get(to.getChunk().getChunkKey());
 
         if (fromUUID != null && !fromUUID.equals(toUUID)) {
             Nation nation = PersistentData.instance.getNations().get(fromUUID);
-            event.getPlayer().sendMessage(Language.format(event.getPlayer(), "prefix") + " " + nation.getMessages().get("exit"));
+            player.sendMessage(Language.format(player, "prefix") + " " + nation.getMessages().get("exit"));
         }
 
         if (toUUID != null && !toUUID.equals(fromUUID)) {
             Nation nation = PersistentData.instance.getNations().get(toUUID);
-            event.getPlayer().sendMessage(Language.format(event.getPlayer(), "prefix") + " " + nation.getMessages().get("entry"));
+            player.sendMessage(Language.format(player, "prefix") + " " + nation.getMessages().get("entry"));
         }
     }
 }
